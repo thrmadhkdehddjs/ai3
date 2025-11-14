efabc909328fb29f24783ed845aedda373eed649
thrmadhkdehddjs <H098S02621@chunchon.gwe.hs.kr>
2025-11-14 15:26:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labels[0]: {
        "texts": ["발로란트 가디언", "국물 맛 포인트", "지역별 스타일 차이"],
        "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-       "videos": ["https://youtu.be/XXXXXXXXXXX"]
+       "videos": ["https://www.youtube.com/watch?v=j5UOdqtOudc"]
      },
 }
 
